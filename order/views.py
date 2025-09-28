fdfaeb68022fa9c33ca6ebe11618d9c4927c061e
tanzina <tn59249@yahoo.com>
2025-09-28 20:02:26 +0600

    CartViewSet updated for create or get cart

--- order/views.py
+++ order/views.py
@@ -17,7 +17,7 @@ from rest_framework.decorators import action
 
 from order.services import OrderService
 from rest_framework.response import Response
-
+from rest_framework import status
 
 # Create your views here.
 
@@ -38,6 +38,15 @@ class CartViewSet(
         return Cart.objects.prefetch_related("items__product").filter(
             user=self.request.user
         )
+
+    def create(self, request, *args, **kwargs):
+        existing_cart = Cart.objects.filter(user=request.user).first()
+
+        if existing_cart:
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return super().create(request, *args, **kwargs)
 
 
 class CartItemViewSet(ModelViewSet):
